fcd598744f9839dc288c87358f9c93178ba6c91c
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2025-03-07 23:10:44 -0600

    regresar a los conceptos basicos de js

---
+++ java.js
@@ -0,0 +1,11 @@
+let originalArray = [1, 2, 3, 4, 5];
+let newArray = originalArray.filter(num => num !== 5); // Creates a new array without 5
+console.log(originalArray); // Output: [1, 2, 3, 4, 5]
+console.log(newArray); // Output: [1, 2, 3, 4]
+
+//splice modifica el array original.
+
+
+//usar delete con su propia asignacion y luego comparar la matriz y luego eliminar ese valor de la matriz.
+
+//partiremos desde la forma mas basica de js - asinacion de valor por numeros o strings
